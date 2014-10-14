7cee0f19f622218528ea244db80c5f51f3918214
Alovchin91 <alovchin91@gmail.com>
2014-10-14 14:17:58 +0000

    Windows Phone example skeleton

---
+++ examples/windows_phone/example.app/App.xaml.cs
@@ -0,0 +1,152 @@
+﻿/* libFLAC_winrt - FLAC library for Windows Runtime
+ * Copyright (C) 2014  Alexander Ovchinnikov
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * - Neither the name of copyright holder nor the names of project's
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace FLAC_WinRT.Example.App
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public sealed partial class App
+    {
+        private TransitionCollection transitions;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += this.OnSuspending;
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used when the application is launched to open a specific file, to display
+        /// search results, and so forth.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                this.DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                // TODO: change this value to a cache size that is appropriate for your application
+                rootFrame.CacheSize = 1;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    // TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // Removes the turnstile navigation for startup.
+                if (rootFrame.ContentTransitions != null)
+                {
+                    this.transitions = new TransitionCollection();
+                    foreach (var c in rootFrame.ContentTransitions)
+                    {
+                        this.transitions.Add(c);
+                    }
+                }
+
+                rootFrame.ContentTransitions = null;
+                rootFrame.Navigated += this.RootFrame_FirstNavigated;
+
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                if (!rootFrame.Navigate(typeof(MainPage), e.Arguments))
+                {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        /// <summary>
+        /// Restores the content transitions after the app has launched.
+        /// </summary>
+        /// <param name="sender">The object where the handler is attached.</param>
+        /// <param name="e">Details about the navigation event.</param>
+        private void RootFrame_FirstNavigated(object sender, NavigationEventArgs e)
+        {
+            var rootFrame = sender as Frame;
+            rootFrame.ContentTransitions = this.transitions ?? new TransitionCollection() { new NavigationThemeTransition() };
+            rootFrame.Navigated -= this.RootFrame_FirstNavigated;
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+
+            // TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
---
+++ examples/windows_phone/example.task/BackgroundAudioTask.cs
@@ -0,0 +1,153 @@
+﻿/* libFLAC_winrt - FLAC library for Windows Runtime
+ * Copyright (C) 2014  Alexander Ovchinnikov
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * - Neither the name of copyright holder nor the names of project's
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.Media.Playback;
+
+namespace FLAC_WinRT.Example.Task
+{
+    public sealed class BackgroundAudioTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral _taskDeferral;
+        private SystemMediaTransportControls _mediaTransportControls;
+        private readonly List<string> _trackList;
+        //private MediaSourceAdapter _mediaSourceAdapter;
+
+        public BackgroundAudioTask()
+        {
+            this._trackList = new List<string>();
+        }
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            this._mediaTransportControls = SystemMediaTransportControls.GetForCurrentView();
+
+            this._mediaTransportControls.ButtonPressed += this.OnMediaTransportControlsButtonPressed;
+            this._mediaTransportControls.IsEnabled = true;
+            this._mediaTransportControls.IsPlayEnabled = true;
+            this._mediaTransportControls.IsPauseEnabled = true;
+            this._mediaTransportControls.DisplayUpdater.ClearAll();
+            this._mediaTransportControls.DisplayUpdater.Type = MediaPlaybackType.Music;
+            this._mediaTransportControls.DisplayUpdater.Update();
+
+            taskInstance.Canceled += this.OnTaskCanceled;
+            taskInstance.Task.Completed += this.OnTaskCompleted;
+
+            BackgroundMediaPlayer.Current.AutoPlay = true;
+            BackgroundMediaPlayer.Current.CurrentStateChanged += this.OnCurrentStateChanged;
+            BackgroundMediaPlayer.MessageReceivedFromForeground += this.OnMessageReceivedFromForeground;
+
+            BackgroundMediaPlayer.SendMessageToForeground(new ValueSet { { "BackgroundPlayerStarted", null } });
+
+            this._taskDeferral = taskInstance.GetDeferral();
+        }
+
+        private void OnMessageReceivedFromForeground(object sender, MediaPlayerDataReceivedEventArgs e)
+        {
+            foreach (var data in e.Data.Where(d => d.Value is string))
+            {
+                if (data.Key.Equals("AddTrack"))
+                {
+                    this._trackList.Add(data.Value as string);
+                }
+            }
+
+            if (!this._trackList.Any())
+            {
+                return;
+            }
+
+            var currentState = BackgroundMediaPlayer.Current.CurrentState;
+            if (currentState == MediaPlayerState.Closed || currentState == MediaPlayerState.Stopped)
+            {
+                var firstTrack = this._trackList.First();
+                //this._mediaSourceAdapter = MediaSourceFactory.Create(firstTrack);
+                //BackgroundMediaPlayer.Current.SetMediaSource(this._mediaSourceAdapter.MediaSource);
+            }
+        }
+
+        private void OnCurrentStateChanged(MediaPlayer sender, object args)
+        {
+            switch (sender.CurrentState)
+            {
+                case MediaPlayerState.Playing:
+                    this._mediaTransportControls.PlaybackStatus = MediaPlaybackStatus.Playing;
+                    break;
+                case MediaPlayerState.Paused:
+                    this._mediaTransportControls.PlaybackStatus = MediaPlaybackStatus.Paused;
+                    break;
+                case MediaPlayerState.Stopped:
+                    this._mediaTransportControls.PlaybackStatus = MediaPlaybackStatus.Stopped;
+                    break;
+                case MediaPlayerState.Closed:
+                    this._mediaTransportControls.PlaybackStatus = MediaPlaybackStatus.Closed;
+                    break;
+            }
+        }
+
+        private void OnMediaTransportControlsButtonPressed(SystemMediaTransportControls sender, SystemMediaTransportControlsButtonPressedEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case SystemMediaTransportControlsButton.Play:
+                    BackgroundMediaPlayer.Current.Play();
+                    break;
+                case SystemMediaTransportControlsButton.Pause:
+                    BackgroundMediaPlayer.Current.Pause();
+                    break;
+            }
+        }
+
+        private void OnTaskCompleted(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs e)
+        {
+            this._taskDeferral.Complete();
+        }
+
+        private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            this._mediaTransportControls.ButtonPressed -= this.OnMediaTransportControlsButtonPressed;
+
+            sender.Canceled -= this.OnTaskCanceled;
+            sender.Task.Completed -= this.OnTaskCompleted;
+
+            BackgroundMediaPlayer.Current.CurrentStateChanged -= this.OnCurrentStateChanged;
+            BackgroundMediaPlayer.MessageReceivedFromForeground -= this.OnMessageReceivedFromForeground;
+
+            BackgroundMediaPlayer.Shutdown();
+
+            this._taskDeferral.Complete();
+        }
+    }
+}
